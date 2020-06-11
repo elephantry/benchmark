535dc5c34f2a0e3250c80f97922b6b90a1efd49e
Georg Semmler <georg_semmler_05@web.de>
2020-06-11 13:51:25 +0200

    Improve postgres benchmarks
    * Add a helper to construct the user struct from a row
    * Do batch inserts to speedup setting up the database
    * Use explicit select clauses everywhere
    * Load only what is required for `fetch_first` and `fetch_last`

--- src/postgres.rs
+++ src/postgres.rs
@@ -1,8 +1,21 @@
+use postgres::Row;
+
 struct User {
     id: i32,
     name: String,
     hair_color: Option<String>,
     created_at: chrono::NaiveDateTime,
+}
+
+impl User {
+    fn from_row(row: &Row) -> Self {
+        User {
+            id: row.get("id"),
+            name: row.get("name"),
+            hair_color: row.get("hair_color"),
+            created_at: row.get("created_at"),
+        }
+    }
 }
 
 fn setup() -> Result<postgres::Client, postgres::Error> {
@@ -29,31 +42,77 @@ fn tear_down(client: &mut postgres::Client) -> Result<(), postgres::Error> {
     Ok(())
 }
 
-fn insert(client: &mut postgres::Client, n: usize) -> Result<(), postgres::Error> {
+fn insert_users(client: &mut postgres::Client, n: usize) -> Result<(), postgres::Error> {
+    let mut query = String::from("INSERT INTO users (name, hair_color) VALUES");
+    let mut params = Vec::with_capacity(2 * n);
     for x in 0..n {
-        client.execute(
-            "INSERT INTO users (name, hair_color) VALUES ($1, $2)",
-            &[&format!("User {}", x), &format!("hair color {}", x)],
-        )?;
+        query += &format!(
+            "{} (${}, ${})",
+            if x == 0 { "" } else { "," },
+            2 * x + 1,
+            2 * x + 2,
+        );
+        params.push(format!("User {}", x));
+        params.push(format!("hair color {}", x));
+    }
+    let params = params.iter().map(|p| p as _).collect::<Vec<_>>();
+    client.execute(&query as &str, &params)?;
+
+    Ok(())
+}
+
+fn insert_posts(
+    client: &mut postgres::Client,
+    post_count_per_user: i32,
+) -> Result<(), postgres::Error> {
+    let user_ids = client
+        .query("SELECT id FROM users", &[])?
+        .into_iter()
+        .map(|row| row.get("id"))
+        .collect::<Vec<i32>>();
+
+    let mut query = String::from("INSERT INTO posts (title, content, author) VALUES");
+
+    let mut counter: i32 = 0;
+    let mut params = Vec::with_capacity(user_ids.len() * post_count_per_user as usize);
+    for user_id in user_ids {
+        for post in 0..post_count_per_user {
+            query += &format!(
+                "{} (${}, ${}, ${})",
+                if counter == 0 { "" } else { "," },
+                3 * counter + 1,
+                3 * counter + 2,
+                3 * counter + 3
+            );
+            counter += 1;
+            params.push((
+                format!("Post number {} for user {}", post, user_id),
+                "abc".chars().cycle().take(1).collect::<String>(),
+                user_id,
+            ));
+        }
     }
 
+    let params = params
+        .iter()
+        .flat_map(|(title, content, author)| vec![title as _, content as _, author as _])
+        .collect::<Vec<_>>();
+
+    client.execute(&query as &str, &params)?;
     Ok(())
 }
 
 #[bench]
 fn query_one(b: &mut test::Bencher) -> Result<(), postgres::Error> {
     let mut client = setup()?;
-    insert(&mut client, 1)?;
+    insert_users(&mut client, 1)?;
 
     b.iter(|| {
-        let stmt = client.prepare("SELECT * FROM users LIMIT 1").unwrap();
+        let stmt = client
+            .prepare("SELECT id, name, hair_color, created_at FROM users LIMIT 1")
+            .unwrap();
         let rows = client.query(&stmt, &[]).unwrap();
-        User {
-            id: rows[0].get("id"),
-            name: rows[0].get("name"),
-            hair_color: rows[0].get("hair_color"),
-            created_at: rows[0].get("created_at"),
-        }
+        User::from_row(&rows[0])
     });
 
     tear_down(&mut client)
@@ -62,19 +121,14 @@ fn query_one(b: &mut test::Bencher) -> Result<(), postgres::Error> {
 #[bench]
 fn query_all(b: &mut test::Bencher) -> Result<(), postgres::Error> {
     let mut client = setup()?;
-    insert(&mut client, 10_000)?;
+    insert_users(&mut client, 10_000)?;
 
     b.iter(|| {
         client
-            .query("SELECT * FROM users", &[])
+            .query("SELECT id, name, hair_color, created_at FROM users", &[])
             .unwrap()
             .iter()
-            .map(|row| User {
-                id: row.get("id"),
-                name: row.get("name"),
-                hair_color: row.get("hair_color"),
-                created_at: row.get("created_at"),
-            })
+            .map(User::from_row)
             .collect::<Vec<_>>()
     });
 
@@ -86,7 +140,12 @@ fn insert_one(b: &mut test::Bencher) -> Result<(), postgres::Error> {
     let mut client = setup()?;
 
     b.iter(|| {
-        insert(&mut client, 1).unwrap();
+        client
+            .execute(
+                "INSERT INTO users (name, hair_color) VALUES ($1, $2)",
+                &[&"User 0", &"hair color 0"],
+            )
+            .unwrap()
     });
 
     tear_down(&mut client)
@@ -95,21 +154,16 @@ fn insert_one(b: &mut test::Bencher) -> Result<(), postgres::Error> {
 #[bench]
 fn fetch_first(b: &mut test::Bencher) -> Result<(), postgres::Error> {
     let mut client = setup()?;
-    insert(&mut client, 10_000)?;
+    insert_users(&mut client, 10_000)?;
 
     b.iter(|| {
         client
-            .query("SELECT * FROM users", &[])
+            .query("SELECT id, name, hair_color, created_at FROM users", &[])
             .unwrap()
             .iter()
-            .map(|row| User {
-                id: row.get("id"),
-                name: row.get("name"),
-                hair_color: row.get("hair_color"),
-                created_at: row.get("created_at"),
-            })
+            .map(User::from_row)
             .next()
-            .unwrap();
+            .unwrap()
     });
 
     tear_down(&mut client)
@@ -118,21 +172,16 @@ fn fetch_first(b: &mut test::Bencher) -> Result<(), postgres::Error> {
 #[bench]
 fn fetch_last(b: &mut test::Bencher) -> Result<(), postgres::Error> {
     let mut client = setup()?;
-    insert(&mut client, 10_000)?;
+    insert_users(&mut client, 10_000)?;
 
     b.iter(|| {
         client
-            .query("SELECT * FROM users", &[])
+            .query("SELECT id, name, hair_color, created_at FROM users", &[])
             .unwrap()
             .iter()
-            .map(|row| User {
-                id: row.get("id"),
-                name: row.get("name"),
-                hair_color: row.get("hair_color"),
-                created_at: row.get("created_at"),
-            })
+            .map(User::from_row)
             .nth(9_999)
-            .unwrap();
+            .unwrap()
     });
 
     tear_down(&mut client)
