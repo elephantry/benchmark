26b248517fe91afba0af4fc655ada8fb3815de65
Georg Semmler <georg_semmler_05@web.de>
2020-06-11 14:05:54 +0200

    Allow disabling some benchmarks via features

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,18 @@
 #![allow(soft_unstable)]
 #![allow(dead_code)]
 
-#[macro_use]
+#[cfg_attr(feature = "diesel", macro_use)]
+#[cfg(feature = "diesel")]
 extern crate diesel;
 extern crate test;
 
+#[cfg(feature = "diesel")]
 mod diesel_;
+#[cfg(feature "elephantry")]
 mod elephantry;
+#[cfg(feature = "postgres")]
 mod postgres;
+#[cfg(feature = "sqlx")]
 mod sqlx;
 
 trait Client: Sized {
